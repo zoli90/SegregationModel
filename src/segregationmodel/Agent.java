48f58608047b08b36792e79292179cf1de31e493
zoli90 <>
2021-01-24 22:30:05 +0100

    Getting prepared to run a simulation with some hardcoded inputs.

--- src/segregationmodel/Agent.java
+++ src/segregationmodel/Agent.java
@@ -5,6 +5,8 @@
  */
 package segregationmodel;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
 
 /**
@@ -13,9 +15,19 @@ import java.util.ArrayList;
  */
 public class Agent {
     
+    //Section 1: instance variables
+    
     private float homophility; //float between 0 and 1 that shows the attraction to agents of the same type (or more precisely: the repulsion by different agents)
+    
+    private ArrayList<Cell> neighborCells = new ArrayList<>(); //a dynamic list of neighboring cells. The number of cells vary because at the edge of the map the agent has less than 8 neighboring cells.
+
     private Cell cell = new Cell(0,0,'A'); //an initialized cell object that will store the agent's color and coordinates.
-    ArrayList<Cell> neighborCells = new ArrayList<>(); //a dynamic list of neighboring cells. The number of cells vary because at the edge of the map the agent has less than 8 neighboring cells.
+    
+    private Cell nextStep = null; //output for deciding where to move and input for changing position. After moving (or if there's nowhere to go) it is set back to null.
+    
+    public static Random randomGenerator = new Random(SegregationModel.seed);
+    
+    //Section 2: basic setters
     
     public void setColor(char colorInput) {
         if ((int) colorInput >= 65 && (int) colorInput <= SegregationModel.LAST_COLOR)
@@ -23,8 +35,10 @@ public class Agent {
     }
     
     public void setHomophility(float homophilityInput) {
+        
         if (homophilityInput >= 0f && homophilityInput <= 1f)
             homophility = homophilityInput;
+        
     }    
     
     public void setX(int x) {
@@ -34,6 +48,8 @@ public class Agent {
     public void setY(int y) {
         cell.y = y;
     }
+    
+    //Section 3: Methods that provide input for the Agent to process
     
     //this is a test method that can be inserted in methods manipulating ArrayList<Cell>s.
     
@@ -76,11 +92,12 @@ public class Agent {
             for (int j = Math.max(0, cell.y - 1) ; j < Math.min(cell.y + 2, arr.length); j++) {
                 
                 if (!(i == cell.x && j == cell.y)) //skip the Agent's own position
-                    neighborCells.add(new Cell(i, j, arr[i][j]));
+                    neighborCells.add(new Cell(i, j, arr[j][i]));
                     
             }
         }
-        checkCells(neighborCells);
+        if (checkCells(neighborCells) != checkCellCode.ARRAYLIST_OK)
+            System.out.println("getCells method has created duplicate cells!");
     }
 
     public int getEmptyCells() {
@@ -92,6 +109,83 @@ public class Agent {
         }
         
         return output;
+        
+    }
+    
+    public int getSameColorCells() {
+        
+        int output = 0;
+        for (Cell cells : neighborCells) {
+            if (cells.color == cell.color)
+                output++;
+        }
+        
+        return output;
+        
+    }
+    
+    public int getDifferentColorCells() {
+        
+        int output = neighborCells.size() - getEmptyCells() - getSameColorCells();
+        return output;
+        
+    }
+    
+    //Section 4: Decision making methods
+    
+    //getNextStep and getNeighborCells are just for testing purposes
+    
+    public Cell getNextStep() {
+        return nextStep;
+    }
+    
+    public ArrayList<Cell> getNeighborCells() {
+        return neighborCells;
+    }
+    
+    public void setNextStep() {
+        
+        int emptyCells = getEmptyCells();
+        int sameColor = getSameColorCells();
+        int differentColor = getDifferentColorCells();
+        float colorRatio = (float) sameColor / (sameColor + differentColor);
+        
+        if (emptyCells == 0) {
+            nextStep = null;
+        } else if (colorRatio >= homophility) {
+            nextStep = null;
+        } else {
+            removeOccopiedCells();
+            nextStep = neighborCells.get(randomGenerator.nextInt(neighborCells.size()));
+        }
+        
+    }
+    
+    public void removeOccopiedCells() {
+        
+        Iterator<Cell> it = neighborCells.iterator();
+        while (it.hasNext()) {
+            Cell testedCell = it.next();
+            if (testedCell.color != '_')
+                it.remove();
+        }
+        
+        if (checkCells(neighborCells) != checkCellCode.ARRAYLIST_OK)
+            System.out.println("getCells method has created duplicate cells!");        
+        
+    }
+    
+    //Section 5: change position, update everything that's needed
+    
+    public char[][] setPosition(char[][] arr) {
+        
+        arr[nextStep.y][nextStep.x] = cell.color;
+        arr[cell.y][cell.x] = '_';
+        cell.x = nextStep.x;
+        cell.y = nextStep.y;
+        nextStep = null;
+        
+        return arr;
         
     }
     
